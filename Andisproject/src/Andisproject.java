37a7fd7ab86043af3c7d2d0fef2927d34a0274bf
Hetzel.Andreas <Hetzel.Andreas@SK-448A5BCB8AD0.2631.skool.net>
2018-09-24 14:20:13 +0200

    primzahl counter

--- Andisproject/src/Andisproject.java
+++ Andisproject/src/Andisproject.java
@@ -1,4 +1,59 @@
 
 public class Andisproject {
+     
+   	 public static int counter =0;
+    		public static void main(String[] args) {
+    			
+    			testRow(1000);
+    			sub(10,5);
+    			
+    			  
+    			  
+    			
+    			}
+    			
+    			public static boolean testSingleNumber(int n){
+    				 boolean prim = true;
+    				 int i = 2;
+    				 
+    				  if(n == 1) {
+    	    			    prim = false;
+    	    			  }
+    	    			  
+    	    			  else{
+    	    			    while (i <= n-1 && prim == true) {
+    	    			    	 if (n%i==0) {
+    	    	    			        prim = false;
+    	    	    			      }
+    	    	    			      
+    	    	    			      i = i+1;
+    	    	    			    }
+    	    	    			  }
+    	    	    			 
+    				 
+    				 
+    				 if(prim){ausgabePrim(n);}
+    				 
+    				 return prim;
+    			}
+    			
+    			public static void ausgabePrim(int n){
+    			 
+    			System.out.print(n+"");
+    			counter++;
+    			if(counter%20==0){
+    				System.out.println();
+    			}
+    			}
+    		public static void testRow(int threshold){
+			for(int i = 2; i<=threshold; i++){
+				testSingleNumber(i);
+            }
+    		}
 
+    		public static int sub(int x, int y){
+    			ausgabePrim(x/y);
+    		return x+y;
+    			
+    	}
 }
